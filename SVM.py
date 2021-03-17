16398cb1bdd27ef211bee271b7f2e51d56e6f8e4
Mia Wei <miawei.mwei@gmail.com>
2021-03-17 10:25:32 -0400

    Added ROC plot

--- SVM.py
+++ SVM.py
@@ -48,13 +48,16 @@ print('\nRemove outliers', X.shape, y.shape)
 selector = SelectKBest(k=10)
 X = selector.fit_transform(X,y)
 print('\nFeature selection', X.shape, y.shape)
-print(selector.scores_)
+#print(selector.scores_)
 #print(selector.pvalues_)
 
 ###################### Split Dataset ###########################
 # split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 print('\nTraining set',X_train.shape, y_train.shape)
+
+# generate a no skill prediction (majority class)
+ns_probs = [0 for _ in range(len(y_test))]
 
 ###################### Training Dataset ###########################
 # Standarize all features
@@ -63,53 +66,82 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 # Resample the imbalance dataset by using SMOTEENN
-# model_smote = SMOTE(random_state = 42) 
-# X_train, y_train = model_smote.fit_sample(X_train, y_train) 
-# print('Training set', X_train.shape, y_train.shape)
-# counter = Counter(y_train)
-# print('After oversampling',Counter(y_train))
+model_smote = SMOTE(random_state = 42) 
+X_train, y_train = model_smote.fit_sample(X_train, y_train) 
+print('Training set', X_train.shape, y_train.shape)
+counter = Counter(y_train)
+print('After oversampling',Counter(y_train))
 
 # Resample the imbalance dataset by undersampling
-from imblearn.under_sampling import TomekLinks
-from imblearn.under_sampling import CondensedNearestNeighbour
-from imblearn.under_sampling import NearMiss
-from imblearn.under_sampling import ClusterCentroids
+# from imblearn.under_sampling import TomekLinks
+# from imblearn.under_sampling import CondensedNearestNeighbour
+# from imblearn.under_sampling import NearMiss
+# from imblearn.under_sampling import ClusterCentroids
 
-counter = Counter(y_train)
-print('Before undersampling', counter)
+# counter = Counter(y_train)
+# print('Before undersampling', counter)
 
-# define the undersampling method
-#undersample = TomekLinks()
-#undersample = CondensedNearestNeighbour(n_neighbors=1)
-undersample = NearMiss(version=3, n_neighbors_ver3=3)
-#undersample = ClusterCentroids(random_state=0)
+# # define the undersampling method
+# #undersample = TomekLinks()
+# #undersample = CondensedNearestNeighbour(n_neighbors=1)
+# #undersample = NearMiss(version=3, n_neighbors_ver3=3)
+# #undersample = ClusterCentroids(random_state=0)
 
-# transform the dataset
-X_train, y_train = undersample.fit_resample(X_train, y_train)
+# # transform the dataset
+# #X_train, y_train = undersample.fit_resample(X_train, y_train)
 
-# summarize the new class distribution
-counter = Counter(y_train)
-print('After undersampling', counter)
+# # summarize the new class distribution
+# counter = Counter(y_train)
+# print('After undersampling', counter)
 
 # Train the SVM model on the Training set
 #classifier = SVC(kernel='linear', class_weight = 'balanced', C=0.1, random_state = 42)
-classifier = SVC(kernel='poly', class_weight = 'balanced', gamma = 0.3, C=100, random_state = 42)
+#classifier = SVC(kernel='linear', decision_function_shape = 'ovo', shrinking = False, cache_size = 10000, verbose = True, max_iter = -1, random_state = 0)
+classifier = SVC(kernel='rbf', class_weight='balanced', decision_function_shape = 'ovo', shrinking = False, probability=True, cache_size = 10000, verbose = True, random_state = 0)
 classifier.fit(X_train, y_train)
 
 ###################### Test Dataset ###########################
 # Predicte the Test set results
-print(X_test.shape)
+print('\nTest data', X_test.shape)
 y_pred = classifier.predict(X_test) 
 
-###################### Evaluation Dataset ###########################
-# Evaluate predictions
-mae = mean_absolute_error(y_test, y_pred)
-print('MAE: %.3f' % mae)
-tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-print('TN =', tn, 'FP =', fp, 'FN =', fn, 'TP =', tp)
-print(classification_report(y_test,y_pred))
+# predict probabilities
+svm_probs = classifier.predict_proba(X_test)
+# keep probabilities for the positive outcome only
+svm_probs = svm_probs[:, 1]
+
+######################       ROC/AUC   ###########################
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+# calculate scores
+ns_auc = roc_auc_score(y_test, ns_probs)
+lr_auc = roc_auc_score(y_test, svm_probs)
+# summarize scores
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('SVM: ROC AUC=%.3f' % (lr_auc))
+# calculate roc curves
+ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+svm_fpr, scm_tpr, _ = roc_curve(y_test, svm_probs)
+# plot the roc curve for the model
+plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+plt.plot(svm_fpr, scm_tpr, marker='.', label='SVM')
+# axis labels
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+# show the legend
+plt.legend()
+# show the plot
+plt.show()
 
 # ROC/AUC
 FP_rate, TP_rate, thresholds = roc_curve(y_test, y_pred)
+print(thresholds)
 roc_auc = auc(FP_rate, TP_rate)
 print('roc_auc', roc_auc)
+
+###################### Evaluation Dataset ###########################
+# Evaluate predictions
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+print('TN =', tn, 'FP =', fp, 'FN =', fn, 'TP =', tp)
+print('ACC =', (tp+tn)/(tp+tn+fn+fp))
+print(classification_report(y_test,y_pred))
