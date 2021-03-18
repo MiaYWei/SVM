7ffcc5293a5a14f87170fda711bb6c4398493ba9
Mia Wei <miawei.mwei@gmail.com>
2021-03-17 20:36:15 -0400

    Updated code

--- SVM.py
+++ SVM.py
@@ -14,15 +14,9 @@ from collections import Counter
 from sklearn.metrics import roc_curve, auc
 
 # Import the dataset
-dataset = pd.read_csv('csv_result-Descriptors_Calibration.csv') 
+dataset = pd.read_csv('csv_result-Descriptors_Training.csv') 
 X = dataset.iloc[:, 1:-1]
 y = dataset.iloc[:, -1]
-
-# # Taking care of missing data
-# from sklearn.impute import SimpleImputer
-# imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-# imputer.fit(X) 
-# X = imputer.transform(X)
 
 # Convert 'P, N' into '1, 0'
 methylated = np.zeros(len(y))
@@ -36,7 +30,7 @@ y = methylated
 print('Original dataset', X.shape, y.shape)
 print(Counter(y))
 
-###################### Split Dataset ###########################
+###################### Dataset Pre-processing ###########################
 # Identify outliers in the training dataset
 z_scores = stats.zscore(X)
 abs_z_scores = np.abs(z_scores)
@@ -46,7 +40,7 @@ y = y[filtered_entries]
 print('\nRemove outliers', X.shape, y.shape)
 
 # Select features
-selector = SelectKBest(k=5)
+selector = SelectKBest(k=10)
 X = selector.fit_transform(X,y)
 print('\nFeature selection', X.shape, y.shape)
 #print(selector.scores_)
@@ -66,39 +60,15 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Resample the imbalance dataset by using SMOTEENN
+# Resample the imbalance dataset by using SMOTE
 model_smote = SMOTE(random_state = 42) 
 X_train, y_train = model_smote.fit_sample(X_train, y_train) 
 print('Training set', X_train.shape, y_train.shape)
 counter = Counter(y_train)
 print('After oversampling',Counter(y_train))
 
-# Resample the imbalance dataset by undersampling
-# from imblearn.under_sampling import TomekLinks
-# from imblearn.under_sampling import CondensedNearestNeighbour
-# from imblearn.under_sampling import NearMiss
-# from imblearn.under_sampling import ClusterCentroids
-
-# counter = Counter(y_train)
-# print('Before undersampling', counter)
-
-# # define the undersampling method
-# #undersample = TomekLinks()
-# #undersample = CondensedNearestNeighbour(n_neighbors=1)
-# #undersample = NearMiss(version=3, n_neighbors_ver3=3)
-# #undersample = ClusterCentroids(random_state=0)
-
-# # transform the dataset
-# #X_train, y_train = undersample.fit_resample(X_train, y_train)
-
-# # summarize the new class distribution
-# counter = Counter(y_train)
-# print('After undersampling', counter)
-
 # Train the SVM model on the Training set
-#classifier = SVC(kernel='linear', class_weight = 'balanced', C=0.1, random_state = 42)
-#classifier = SVC(kernel='linear', decision_function_shape = 'ovo', shrinking = False, cache_size = 10000, verbose = True, max_iter = -1, random_state = 0)
-classifier = SVC(kernel='rbf', class_weight='balanced', decision_function_shape = 'ovo', shrinking = False, probability=True, cache_size = 10000, verbose = False, random_state = 0)
+classifier = SVC(kernel='rbf', class_weight='balanced', decision_function_shape = 'ovo', shrinking = False, probability=True, verbose = False, random_state = 0)
 classifier.fit(X_train, y_train)
 
 ###################### Test Dataset ###########################
@@ -160,3 +130,9 @@ tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 print('TN =', tn, 'FP =', fp, 'FN =', fn, 'TP =', tp)
 print('ACC =', (tp+tn)/(tp+tn+fn+fp))
 print(classification_report(y_test,y_pred))
+
+
+###################### Bagging ###########################
+# from sklearn.ensemble import BaggingClassifier
+# ensemble = BaggingClassifier(base_estimator=classifier, n_estimators=31, random_state=42)
+# ensemble.fit(X, y)
