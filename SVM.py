b3cfa4cd1324b6aa2bb8265017b6cb645a826953
Mia Wei <miawei.mwei@gmail.com>
2021-03-19 00:44:02 -0400

    Max Pr@Re50 0.1106

--- SVM.py
+++ SVM.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, mean_absolute_error
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE, SVMSMOTE
+from imblearn.over_sampling import SVMSMOTE
 from sklearn.feature_selection import SelectKBest
 import matplotlib.pyplot as plt
 from collections import Counter
@@ -58,22 +58,12 @@ X = X[filtered_entries]
 y = y[filtered_entries]
 print('\nRemove outliers', X.shape, y.shape)
 
-# # Select features
-# selector = SelectKBest(k = 10)
-# X = selector.fit_transform(X,y)
-# print('\nFeature selection', X.shape, y.shape)
-# # print(selector.scores_)
-# # print(selector.pvalues_)
-
-#features_selected = ['ECI_IB_4_N1','Gs(U)_IB_68_N1', 'Gs(U)_NO_ALR_SI71','ISA_NO_NPR_S','IP_NO_PLR_S']
+# Select features
 features_selected = ['ECI_IB_4_N1','Gs(U)_IB_68_N1', 'Gs(U)_IB_60_N1', 'Z1_NO_sideR35_CV', 'Gs(U)_NO_ALR_SI71','ISA_NO_NPR_S','IP_NO_PLR_S', 'ECI_NO_PCR_CV']
-#features_selected = ['Z1_IB_10_N1','Gs(U)_IB_12_N1','Gs(U)_IB_68_N1', 'Gs(U)_IB_58_N1', 'Gs(U)_IB_60_N1', 'Z1_NO_sideR35_CV', 'Pb_NO_sideR35_S', 'Gs(U)_NO_ALR_SI71', 'IP_NO_PLR_S', 'Pb_NO_PCR_V']
+
 ###################### Split Dataset ###########################
 # Split dataset into train and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-# print('\nTraining set',X_train.shape, y_train.shape)
-
-train, test = train_test_split(data, test_size = 0.2, random_state=100)# in this our main data is splitted into train and test
+train, test = train_test_split(data, test_size = 0.2, random_state = 100)# in this our main data is splitted into train and test
 X_train = train[features_selected]
 y_train=train['class']
 X_test= test[features_selected]
@@ -86,35 +76,32 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 # Resample the imbalance dataset by using SMOTE
-model_smote = SVMSMOTE(sampling_strategy='minority', random_state = 42) 
+model_smote = SVMSMOTE(sampling_strategy='auto', n_jobs = -1, random_state = 42) 
 X_train, y_train = model_smote.fit_sample(X_train, y_train) 
 print('Training set', X_train.shape, y_train.shape)
 print('After oversampling', Counter(y_train))
+
+# The procedure only removes noisy and ambiguous points along the class boundary
+from imblearn.under_sampling import EditedNearestNeighbours
+undersample = EditedNearestNeighbours(n_neighbors=3)
+X_train, y_train = undersample.fit_sample(X_train, y_train)
+print('After undersampling', Counter(y_train))
 
 # Train the SVM model on the Training set
 classifier = SVC(kernel='linear', gamma = 'auto', decision_function_shape = 'ovo', class_weight='balanced', probability=True, shrinking = False, cache_size = 10000, verbose = True, random_state = 42)
 classifier.fit(X_train, y_train)
 
 from sklearn.feature_selection import RFE
-selector = RFE(classifier, 8, step=1)
+selector = RFE(classifier, 8, step = 1)
 selector = selector.fit(X_train, y_train)
-# print(selector.support_)
-# print(selector.ranking_)
-#Result(RFE(classifier, 5, step=1)) -- [22 15 20  9 12  1 18 17  1  5  2 23 24  3  6  7 21 19  1 13  8 10 14  1 16  1 11  4]
-##features_selected = ['ECI_IB_4_N1','Gs(U)_IB_68_N1', 'Gs(U)_NO_ALR_SI71','ISA_NO_NPR_S','IP_NO_PLR_S']
-#Result(RFE(classifier, 8, step=1)) -- [19 12 17  6  9  1 15 14  1  2  1 20 21  1  3  4 18 16  1 10  5  7 11  1 13  1  8  1]
-##features_selected = ['ECI_IB_4_N1','Gs(U)_IB_68_N1', 'Gs(U)_IB_60_N1', 'Z1_NO_sideR35_CV', 'Gs(U)_NO_ALR_SI71','ISA_NO_NPR_S','IP_NO_PLR_S', 'ECI_NO_PCR_CV']
-#selector = RFE(classifier, 10, step=1)
-##[ 9 19  1 12  6  2 15  1  1  1  1 17 18  1  1 10 11  5  1  8  3  4 16 13 7  1  1 14]
+#print(selector.ranking_)
+
 ###################### Test Dataset ###########################
 # Predict the Test set results
 print('\nTest data', X_test.shape)
 y_pred = classifier.predict(X_test) 
 
-# Predict probabilities
-svm_probs = classifier.predict_proba(X_test)
-# keep probabilities for the positive outcome only
-svm_probs = svm_probs[:, 1]
+
 
 ######################       ROC/AUC   ###########################
 from sklearn.metrics import roc_curve
@@ -122,6 +109,11 @@ from sklearn.metrics import roc_auc_score
 
 # Generate a no skill prediction (majority class)
 ns_probs = [0 for _ in range(len(y_test))]
+
+# Predict probabilities
+svm_probs = classifier.predict_proba(X_test)
+# keep probabilities for the positive outcome only
+svm_probs = svm_probs[:, 1]
 
 # Calculate scores
 ns_auc = roc_auc_score(y_test, ns_probs)
@@ -187,8 +179,6 @@ print('Predict Negative', fn+tn)
 print('ACC', (tp+tn)/(tp+tn+fn+fp))
 print('TPR (Sensitivity/Recall)', tp/(tp+fn))
 print('FPR (1-Specificity)', fp/(tn+fp))
-
-print('Precision', tp/(tp+fp))
 
 ###################### Bagging ###########################
 # from sklearn.ensemble import BaggingClassifier
