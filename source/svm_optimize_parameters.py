4611a889f3471a25aa796f4e021b1e563c001ccf
Mia Wei <miawei.mwei@gmail.com>
2021-03-30 22:24:35 -0400

    Added parameter optimization

--- source/svm_optimize_parameters.py
+++ source/svm_optimize_parameters.py
@@ -1,30 +1,26 @@
 # Import the libraries
 import numpy as np
 import pandas as pd
-from scipy import stats
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, mean_absolute_error
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import f_classif,SelectPercentile
 from imblearn.over_sampling import SVMSMOTE
 from imblearn.under_sampling import EditedNearestNeighbours
-import matplotlib.pyplot as plt
-from collections import Counter
-from sklearn.metrics import roc_curve, auc
+from collections import Counter 
+from pandas import read_csv
+from skopt import BayesSearchCV
 
 # Import the dataset
 data = pd.read_csv('csv_result-Descriptors_Calibration.csv') 
-
 # Convert 'P, N' into '1, 0'
 data['class'] = data['class'].map({'P':1,'N':0})
 X = data.iloc[:, 1:-1]
 y = data.iloc[:, -1]
-
 print('Original dataset', X.shape, y.shape, Counter(y))
 
 ###################### Dataset Pre-processing ###########################
-#Identify and remove outliers
+# Identify and remove outliers
 Q1 = X.quantile(0.25)
 Q3 = X.quantile(0.75)
 IQR = Q3 - Q1
@@ -45,34 +41,25 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Resample the imbalance dataset by using SMOTE
+# Resample the imbalance dataset by using SMOTE + ENN
 model_smote = SVMSMOTE(sampling_strategy='auto', n_jobs = -1, random_state = 42) 
 X_train, y_train = model_smote.fit_sample(X_train, y_train) 
-print('Training set', X_train.shape, y_train.shape)
-print('After oversampling', Counter(y_train))
 
-# The procedure only removes noisy and ambiguous points along the class boundary
 undersample = EditedNearestNeighbours(n_neighbors=5)
 X_train, y_train = undersample.fit_sample(X_train, y_train)
-print('After undersampling', Counter(y_train))
 
 ###################### hyperparameters ###########################
-# automatic svm hyperparameter tuning using skopt for the ionosphere dataset
-from pandas import read_csv
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
-from sklearn.model_selection import RepeatedStratifiedKFold
-from skopt import BayesSearchCV
-
+# automatic svm hyperparameter tuning
 # define search space
 params = dict()
 params['C'] = (1e-2, 100.0, 'log-uniform')
 params['gamma'] = (1e-2, 100.0, 'log-uniform')
-params['kernel'] = ['linear']
+params['kernel'] = ['linear', 'rbf']
 # define evaluation
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 # define the search
-search = BayesSearchCV(estimator=SVC(shrinking = False, cache_size = 10000, verbose = True, random_state = 42), search_spaces=params, n_jobs=-1, cv=cv, iid=False)
+search = BayesSearchCV(estimator=SVC(shrinking = False, cache_size = 10000, verbose = True, 
+                       random_state = 42), search_spaces=params, n_jobs=-1, cv=cv, iid=False)
 # perform the search
 search.fit(X_train, y_train)
 # report the best result
